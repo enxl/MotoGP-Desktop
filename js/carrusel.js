616d643c4eb9474d7c1a895672f1ea093407f9a4
enxl <uo287616@uniovi.es>
2025-11-06 10:49:38 +0100

    Carrusel trabajo clase

---
+++ js/carrusel.js
@@ -0,0 +1,12 @@
+class Carrusel {
+    
+    #busqueda;
+    #actual;
+    #maximo;
+
+    constructor() {
+        this.#busqueda = "";
+        this.#actual = 0;
+        this.maximo = 4;
+    }
+}
