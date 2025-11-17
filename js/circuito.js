628fb03caad1ff1d88a0a6569a39683c64017401
enxl <uo287616@uniovi.es>
2025-11-17 20:38:29 +0100

    Mejor carga html

--- js/circuito.js
+++ js/circuito.js
@@ -24,50 +24,13 @@ class Circuito {
                 // Crear sección en este documento!!
                 var main = document.getElementsByTagName("main")[0];
                 var section = document.createElement("section");
+                var contentMain = docExt.getElementsByTagName("main")[0];
+
+                while (contentMain.firstChild) {
+                    section.appendChild(contentMain.firstChild);
+                }
+
                 main.appendChild(section);
-
-                // Obtención de títulos de nivel H3.
-                var titulosH3 = docExt.getElementsByTagName("h3");
-                var h3DatosGenerales = titulosH3[0];
-                var h3Galeria = titulosH3[1];
-                var h3Videos = titulosH3[2];
-                var h3Clasificados = titulosH3[3];
-                var h3Vencedor = titulosH3[4];
-                var h3Referencias = titulosH3[5];
-
-
-                var h2 = docExt.getElementsByTagName("h2")[0];
-                section.appendChild(h2);
-
-                section.appendChild(h3DatosGenerales);
-
-                var parrafos = docExt.getElementsByTagName("p");
-                for (var i = 0; i < parrafos.length; i++) {
-                    var p = document.createElement("p");
-                    p.textContent = parrafos[i].textContent;
-                    section.appendChild(p);
-                }
-
-                section.appendChild(h3Galeria);
-
-                var imagenes = docExt.getElementsByTagName("img");
-                for (var i = 0; i < imagenes.length; i++) {
-                    var img = document.createElement("img");
-                    img.src = imagenes[i].src;
-                    img.alt = imagenes[i].alt;
-                    section.appendChild(img);
-                }
-
-                section.appendChild(h3Videos);
-
-                var videos = docExt.getElementsByTagName("video");
-                for (var i = 0; i < videos.length; i++) {
-                    var video = document.createElement("video");
-                    video.src = videos[i].src;
-                    video.alt = videos[i].alt;
-                    section.appendChild(video);
-                }
-
             };
             lector.readAsText(archivo);
         } else {
